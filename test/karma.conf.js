a1a2741a42fac4f514b604e00e083fda74c1c16a
yutaro <yutaro.tanaka.world@gmail.com>
2015-06-21 18:30:13 +0900

    Set up travis ci

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -69,6 +69,13 @@ module.exports = function (config) {
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
-        singleRun: true
+        singleRun: true,
+        
+        customLaunchers: {
+            Chrome_travis_ci: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        }
     });
 };
