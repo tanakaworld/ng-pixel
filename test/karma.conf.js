a5491e3ef959fcdcd1dd2929fabc6c388b96d56a
yutaro <yutaro.tanaka.world@gmail.com>
2015-06-21 16:56:19 +0900

    Install jasmine helper

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -16,6 +16,8 @@ module.exports = function (config) {
         // list of files / patterns to load in the browser
         files: [
             // Libraries
+            'bower_components/jquery/dist/jquery.js',
+            'bower_components/jasmine-jquery/lib/jasmine-jquery.js',
             'bower_components/angular/angular.js',
             'bower_components/angular-mocks/angular-mocks.js',
 
